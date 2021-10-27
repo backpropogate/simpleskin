b085a8d9ad7c6696d080c52db351fdf1853ac480
wil <Workoutwwill@gmail.com>
2021-10-27 03:02:14 -0400

    first commit

---
+++ next.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    env: {
+        SHOPIFY_STORE_DOMAIN: process.env.SHOPIFY_STORE_DOMAIN,
+        SHOPIFY_STOREFRONT_ACCESSTOKEN: process.env.SHOPIFY_STOREFRONT_ACCESSTOKEN
+    },
+    //since our images are coming from an external source we want to make
+    //sure that they are being optimized
+    images: {
+        domains: ['cdn.shopify.com']
+    }
+
+} 
