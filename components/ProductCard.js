98caa5900ba385da548d0cb058d158469186c4c2
wil <Workoutwwill@gmail.com>
2021-10-28 18:40:56 -0400

    hello commit

--- components/ProductCard.js
+++ components/ProductCard.js
@@ -1,12 +1,14 @@
 import Link from 'next/link'
 import Image from 'next/image'
+import {formatter} from '../utils/helpers'
 
 const ProductCard = ({ product }) => {
     const { handle, title } = product.node
     const {altText, originalSrc } = product.node.images.edges[0].node
+    const price = product.node.priceRange.minVariantPrice.amount
     return (
        <Link
-       href={`/product/${handle}`}
+       href={`/products/${handle}`}
        >
        <a className="group">
            <div className= "w-full bg-gray-200 rounded-3xl overflow-hidden">
@@ -19,6 +21,8 @@ const ProductCard = ({ product }) => {
                    />
                </div>
            </div>
+           <h3 className="mt-4 text-lg font-medium text-gray-900">{title}</h3>
+           <p className="mt-1 text-sm text-gray-700">{formatter.format(price)}</p>
        </a>
 
       </Link>
---
+++ components/ProductPageContent.js
@@ -0,0 +1,11 @@
+
+
+const ProductPageContent = ( { product }) => {
+    return (
+        <div>
+            {product.title}
+        </div>
+    )
+}
+
+export default ProductPageContent
