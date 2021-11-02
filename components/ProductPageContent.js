3910d7e2749a50bbbfe740049b057ad70606c640
wil <Workoutwwill@gmail.com>
2021-11-02 02:09:48 -0400

    final commit

--- components/ProductPageContent.js
+++ components/ProductPageContent.js
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import ProductForm from './ProductForm'
 import { Swiper, SwiperSlide} from 'swiper/react'
 import SwiperCore, { Navigation, Pagination} from 'swiper'
+import RecommendedList from './RecommendedList'
 
 const ProductPageContent = ( { product }) => {
 
@@ -16,6 +17,7 @@ const ProductPageContent = ( { product }) => {
     })
     SwiperCore.use([Navigation, Pagination])
     return (
+        <div>
         <div className="flex flex-col justify-center items-center space-y-8 md:flex-row md:items-start 
         md:space-y-0 md:space-x-4 lg:space-x-8 max-w-6xl w-11/12 mx-auto ">
             <div className="w-full max-w-md border bg-white rounded-2xl overflow-hidden shadow-lg md:w-1/2">
@@ -32,6 +34,9 @@ const ProductPageContent = ( { product }) => {
                 </div>
             </div>
             <ProductForm product={product} />
+        </div>
+        <p className="pt-16 space-y-8 md:space-x-4 lg:space-x-8 max-w-3xl w-11/12 mx-auto">{product.description}</p>
+        <RecommendedList current={product.id} products={product.collections.edges[0].node.products.edges}/>
         </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,13 @@
 import Head from 'next/head'
 import {getProductsInCollection} from '../lib/shopify'
 import ProductList from '../components/ProductList'
+import Hero from '../components/Hero'
 
 export default function Home({products}) {
   
   return (
-    <div>
+    <div className="">
+      <Hero/>
     <ProductList products={products}/>
     </div>
   )
