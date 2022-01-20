22e5b6d7c77ed7c59c3aad8ec0b70fff7cffe69a
wil <Workoutwwill@gmail.com>
2022-01-19 21:23:27 -0500

    second commit

--- components/Hero.js
+++ components/Hero.js
@@ -7,10 +7,14 @@ const Hero = () => {
             
             <div className='sm:px-10 flex flex-col gap-5 sm:justify-evenly '>
                 <h1 className=' text-3xl sm:text-6xl font-bold'>Simple Skincare</h1>
-                <p className='sm:text-2xl max-w-md '>Skincare shouldn't be complicated<br/> so we made it simple</p>
+                <p className='sm:text-2xl max-w-md '>Skincare should not be complicated<br/> so we made it simple</p>
                 <div className='flex gap-3'>
-                    <a href="/#products" className='sm:text-2xl bg-black text-white px-6 py-3 rounded'>Shop Now &rarr;</a>
-                    <a href="/#products" className= ' sm:text-2xl bg-none text-black px-6 py-3 rounded border-2 border-black'>New arrivals</a>
+                    <Link  href="/#products">
+                    <a className='sm:text-2xl bg-black text-white px-6 py-3 rounded'>Shop Now &rarr;</a>
+                    </Link>
+                    <Link href="/#products">
+                    <a  className= ' sm:text-2xl bg-none text-black px-6 py-3 rounded border-2 border-black'>New arrivals</a>
+                    </Link>
                 </div>
             </div>
             <div className='sm:w-1/2 sm:px-10'>
